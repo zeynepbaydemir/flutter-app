3174b2bcf6d3c4cbae39c9a6c4e2e418a927e4e7
banuyildiz <104391422+banuyildiz@users.noreply.github.com>
2022-06-02 22:23:32 +0300

    LoginPage -banuyıldız

---
+++ LoginPage/lib/Header.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Center(
+            child: Text(
+              "GEZY",
+              style: TextStyle(color: Colors.white, fontSize: 40),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Center(
+            child: Text(
+              "hoş geldiniz...",
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ LoginPage/lib/InputField.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class InputField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey))),
+          child: TextField(
+            decoration: InputDecoration(hintText: "E-posta adresi", hintStyle: TextStyle(color: Colors.grey), border: InputBorder.none),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey))),
+          child: TextField(
+            decoration: InputDecoration(hintText: "Şifre", hintStyle: TextStyle(color: Colors.grey), border: InputBorder.none),
+          ),
+        ),
+      ],
+    );
+  }
+}
